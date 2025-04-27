1a61fb543a490c00f4e6a790443daa5cacabfbcc
alekhyamadasu <alekhya1426@gmail.com>
2025-04-27 16:07:12 -0400

    Updated

--- server/index.js
+++ server/index.js
@@ -1,4 +1,35 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
 
+// dotenv.config();
+// const app = express();
+
+// require('dotenv').config();  
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+
+
+// app.use(cors());
+// app.use(express.json());
+
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() =>{ console.log('MongoDB connected');})
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+
+// app.use('/api/devices', require('./routes/devices'));
+// app.use('/api/upload', require('./routes/upload'));
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,13 +40,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-const corsOptions = {
-  origin: 'https://sbom-viewer.onrender.com',  
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
