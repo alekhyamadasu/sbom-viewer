0d39d0474a3d962849a3bfd6d62cc29c9bc21a8b
K Sai Sumanth Reddy <alekhya1426@gmail.com>
2025-05-04 19:52:17 -0400

    Final commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default API_BASE_URL;
+
+// || 'http://localhost:5000'
