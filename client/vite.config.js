58e0a55872cff31369acd255b6762aba9747c19f
K Sai Sumanth Reddy <alekhya1426@gmail.com>
2025-05-03 22:15:57 -0700

    ftgui9pokijuy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5200,
-  },
-  optimizeDeps: {
-    exclude: ['react-router', 'react-router-dom'],
-  },
-  resolve: {
-    alias: {
-      'react-router': '/dev/null', 
-      'react-router-dom': '/dev/null',
-    },
+    port: 5200, 
   },
 });
