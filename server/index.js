ceada88bad0a2f5b9b49fc8abf2a397db31c7171
K Sai Sumanth Reddy <alekhya1426@gmail.com>
2025-05-04 11:49:24 -0700

    Final commit

--- server/index.js
+++ server/index.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-// const app = express();
-
-// require('dotenv').config();  
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-
-
-// app.use(cors());
-// app.use(express.json());
-
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() =>{ console.log('MongoDB connected');})
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-
-// app.use('/api/devices', require('./routes/devices'));
-// app.use('/api/upload', require('./routes/upload'));
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -48,8 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('✅ MongoDB connected'))
-.catch(err => console.error('❌ MongoDB connection error:', err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/devices', require('./routes/devices'));
@@ -57,7 +24,7 @@ app.use('/api/upload', require('./routes/upload'));
 
 // Base route (Important for Vercel health check)
 app.get('/', (req, res) => {
-  res.send('SBOM Backend API is Running ✅');
+  res.send('SBOM Backend API is Running');
 });
 
 // Start server
