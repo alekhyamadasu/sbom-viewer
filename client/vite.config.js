8a4b8a3e4d2450b6645d7e08e8adc7eb4d679812
K Sai Sumanth Reddy <alekhya1426@gmail.com>
2025-05-03 22:38:24 -0700

    dep

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5200, 
+    port: 5200,
+  },
+  resolve: {
+    alias: {
+      'react/jsx-runtime': path.resolve('./node_modules/react/jsx-runtime.js'),
+    },
   },
 });
